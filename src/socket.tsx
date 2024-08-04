0a816246969afc446507c6c0ed16ab7555f20f79
Sumeet <chhinasumeet@gmail.com>
2024-08-04 04:36:14 -0500

    lol

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,7 +1,9 @@
 import { io } from 'socket.io-client';
 
 export const socket = io("wss://unusual-vickie-prj-trex-e25f682f.koyeb.app", {
-    transports: ["websocket"]
+    transports: ["websocket"],
+    reconnection: true,
+    rejectUnauthorized: false
     // secure: true
     // cors: {
     //     origin: "unusual-vickie-prj-trex-e25f682f.koyeb.app/"
